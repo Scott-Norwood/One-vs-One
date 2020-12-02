d54e048455e57266a5512df8570ea90301e1132c
Scott Norwood <weasddddd@gmail.com>
2020-12-02 12:37:02 -0600

    Tilly updated wavemanager class

--- Assets/One vs One (Multiplayer PvE Version)/Scripts/WaveManager.cs
+++ Assets/One vs One (Multiplayer PvE Version)/Scripts/WaveManager.cs
@@ -3,7 +3,6 @@
 // Share — copy and redistribute the material in any medium or format.
 // Adapt — remix, transform, and build upon the material for any purpose, even commercially.
 
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,7 +13,7 @@ namespace xtilly5000.Prototypes.WaveManager
     #region WaveManager Class
     /// <summary>
     /// WaveManager manages the spawning of enemies in the scene.
-    /// This class does NOT manage the contents of the waves, nor does it manage the enemies themselves.
+    /// This class does NOT contain data related to the contents of waves, nor does it manage the enemies themselves.
     /// </summary>
     public class WaveManager : MonoBehaviour
     {
@@ -24,24 +23,24 @@ namespace xtilly5000.Prototypes.WaveManager
         public static event SpawnEnemyDelegate OnSpawnEnemy;
 
         // Create an event system that triggers when a wave starts spawning.
-        public delegate void SpawnWaveDelegate(Wave wave);
-        public static event SpawnWaveDelegate OnSpawnWave;
+        public delegate void WaveStartSpawningDelegate(Wave wave);
+        public static event WaveStartSpawningDelegate OnWaveStartSpawning;
 
         // Create an event system that triggers when a wave finishes spawning.
-        public delegate void SpawnedWaveDelegate(Wave wave);
-        public static event SpawnedWaveDelegate OnSpawnedWave;
+        public delegate void WaveFinishedSpawningDelegate(Wave wave);
+        public static event WaveFinishedSpawningDelegate OnWaveFinishedSpawning;
 
-        // Create an event system that triggers when all enemies are killed.
+        // Create an event system that triggers when all enemies in a wave are killed.
         public delegate void OnWaveKilledDelegate(Wave wave);
         public static event OnWaveKilledDelegate OnWaveKilled;
 
         // Create an event system that triggers when a step starts spawning.
-        public delegate void SpawnStepDelegate(Step step);
-        public static event SpawnStepDelegate OnSpawnStep;
+        public delegate void StepStartSpawningDelegate(Step step);
+        public static event StepStartSpawningDelegate OnStepStartSpawning;
 
         // Create an event system that triggers when a step finishes spawning.
-        public delegate void SpawnedStepDelegate(Step step);
-        public static event SpawnedStepDelegate OnSpawnedStep;
+        public delegate void StepFinishedSpawningDelegate(Step step);
+        public static event StepFinishedSpawningDelegate OnStepFinishedSpawning;
         #endregion
 
         #region Variables
@@ -61,6 +60,7 @@ namespace xtilly5000.Prototypes.WaveManager
         #endregion
 
         #region Awake() Method
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "This function is being used by Unity itself, so we do not need to call it locally.")]
         private void Awake()
         {
             // This is the singleton pattern. We want to make sure that only one WaveManager exists!
@@ -77,21 +77,25 @@ namespace xtilly5000.Prototypes.WaveManager
         }
         #endregion
 
-        #region Start() Method
-        private void Start()
+        #region SpawnWave() Enumerator
+        /// <summary>
+        /// Spawns a wave using a direct reference to the Wave.
+        /// </summary>
+        /// <param name="wave">The wave to spawn.</param>
+        /// <returns>Returns IEnumerator, letting you yield until all the enemies in the wave are killed.</returns>
+        public IEnumerator SpawnWave(Wave wave)
         {
-            // Make sure that you have references to the prefabs, and if you don't, throw an error to let you know you're being silly.
-            if(prefabs.Count != Enum.GetNames(typeof(EnemyType)).Length)
-            {
-                Debug.LogWarning("Prefabs dictionary entry count does not match up with EnemyTypes entry count! Did you forget to add a reference to a prefab, or did you duplicate an entry?");
-            }
+            // Start the Coroutine that will process the wave.
+            yield return StartCoroutine(ProcessWave(wave));
+
+            // Call the proper event for beating a wave.
+            OnWaveKilled?.Invoke(wave);
         }
         #endregion
 
         #region SpawnWave() Enumerator
         /// <summary>
-        /// The more advanced version of SpawnWave(). 
-        /// Only resort to this function unless you have knowledge about how IEnumerators work.
+        /// Spawns a wave using a reference to a Wave in the waves list.
         /// </summary>
         /// <param name="waveNumber">The wave to spawn.</param>
         /// <returns>Returns IEnumerator, letting you yield until all the enemies in the wave are killed.</returns>
@@ -101,6 +105,7 @@ namespace xtilly5000.Prototypes.WaveManager
             if (waves.Count < waveNumber)
             {
                 Debug.LogError("Wave does not exist!");
+                yield break;
             }
 
             // Start the Coroutine that will process the wave.
@@ -121,7 +126,7 @@ namespace xtilly5000.Prototypes.WaveManager
         private IEnumerator ProcessWave(Wave wave)
         {
             // Invoke the proper event for spawning a wave.
-            OnSpawnWave?.Invoke(wave);
+            OnWaveStartSpawning?.Invoke(wave);
 
             // Loop through all of the steps.
             for (int i = 0; i < wave.steps.Count; i++)
@@ -131,12 +136,12 @@ namespace xtilly5000.Prototypes.WaveManager
                 {
                     // Yield return this Coroutine so that the execution of ProcessWave does not finish until the current step finishes execution.
                     yield return StartCoroutine(ProcessStep(wave.steps[i], wave, false));
-                } 
-                else if (i == wave.steps.Count - 1) 
+                }
+                else if (i == wave.steps.Count - 1)
                 {
                     // Yield return this Coroutine so that the execution of ProcessWave does not finish until the current step finishes execution.
                     yield return StartCoroutine(ProcessStep(wave.steps[i], wave, true));
-                } 
+                }
                 else
                 {
                     // We do not want to wait for the step to process before moving onto the next one.
@@ -144,7 +149,7 @@ namespace xtilly5000.Prototypes.WaveManager
                 }
 
                 // Wait the time specified in the current step data before moving onto the next step, unless it is the last step.
-                if(i != wave.steps.Count - 1)
+                if (i != wave.steps.Count - 1)
                 {
                     yield return new WaitForSeconds(wave.steps[i].timingData.timeUntilNextStep);
                 }
@@ -152,7 +157,7 @@ namespace xtilly5000.Prototypes.WaveManager
         }
         #endregion
 
-        #region ProcessStep() Enumerator()
+        #region ProcessStep() Enumerator
         /// <summary>
         /// Processes a step, spawning enemies according to step data.
         /// </summary>
@@ -162,7 +167,7 @@ namespace xtilly5000.Prototypes.WaveManager
         private IEnumerator ProcessStep(Step step, Wave wave, bool lastStep)
         {
             // We want to let the event system know that we started to spawn a step.
-            OnSpawnStep?.Invoke(step);
+            OnStepStartSpawning?.Invoke(step);
 
             // Randomize the enemy count.
             int enemies = Random.Range(step.enemyData.minNumberOfEnemies, step.enemyData.maxNumberOfEnemies);
@@ -171,19 +176,19 @@ namespace xtilly5000.Prototypes.WaveManager
             List<WaveEnemy> references = new List<WaveEnemy>();
 
             // If we have no chance of skipping this step, then don't divide by zero!
-            if(step.skipChance != 0)
+            if (step.skipChance != 0)
             {
-                if(Random.value < step.skipChance / 100)
+                if (Random.value < step.skipChance / 100)
                 {
                     // Break out of this Coroutine and stop execution early.
                     // If this is the last step in the wave, we want to notify the event system.
                     if (lastStep)
                     {
-                        OnSpawnedWave?.Invoke(wave);
+                        OnWaveFinishedSpawning?.Invoke(wave);
                     }
 
                     // Regardless, we want to notify the event system that we finished processing a step.
-                    OnSpawnedStep?.Invoke(step);
+                    OnStepFinishedSpawning?.Invoke(step);
                     yield break;
                 }
             }
@@ -192,36 +197,43 @@ namespace xtilly5000.Prototypes.WaveManager
             for (int i = 0; i < enemies; i++)
             {
                 // Only wait for spacing time after the first iteration.
-                if(i != 0)
+                if (i != 0)
                 {
                     yield return new WaitForSeconds(Random.Range(step.timingData.minSpacing, step.timingData.maxSpacing));
                 }
 
-                // Spawn the enemy and add the references to the global (all waves) and local (current step) reference lists.
-                WaveEnemy enemy = SpawnEnemy(step.enemyData.enemy);
-                references.Add(enemy);
+                if (prefabs.Count <= step.enemyData.id)
+                {
+                    Debug.LogWarning("Could not spawn enemy. The enemy id has no prefab associated with it!");
+                }
+                else
+                {
+                    // Spawn the enemy and add the references to the global (all waves) and local (current step) reference lists.
+                    WaveEnemy enemy = SpawnEnemy(step.enemyData.id);
+                    references.Add(enemy);
 
-                KeyValuePair<Wave, WaveEnemy> pair = new KeyValuePair<Wave, WaveEnemy>(enemy.wave, enemy);
-                enemyReferences.Add(pair);
+                    KeyValuePair<Wave, WaveEnemy> pair = new KeyValuePair<Wave, WaveEnemy>(enemy.wave, enemy);
+                    enemyReferences.Add(pair);
+                }
             }
 
             // If this is the last step in the wave, we want to notify the event system.
             if (lastStep)
-            {   
-                OnSpawnedWave?.Invoke(wave);
+            {
+                OnWaveFinishedSpawning?.Invoke(wave);
             }
 
             // Regardless, we want to notify the event system that we finished processing a step.
-            OnSpawnedStep?.Invoke(step);
+            OnStepFinishedSpawning?.Invoke(step);
 
             // While there are still references in the local (current step) reference list, yield.
-            while(references.Count > 0)
+            while (references.Count > 0)
             {
                 // Loop through all of the references.
                 for (int i = 0; i < references.Count; i++)
                 {
                     // If it's null, then the object was deleted. We also want to check if the object is disabled for object pooling.
-                    if(references[i].obj == null || references[i].obj.activeSelf == false)
+                    if (references[i].obj == null || references[i].obj.activeSelf == false)
                     {
                         // Remove the reference to the deleted object.
                         KeyValuePair<Wave, WaveEnemy> pair = new KeyValuePair<Wave, WaveEnemy>(references[i].wave, references[i]);
@@ -242,14 +254,14 @@ namespace xtilly5000.Prototypes.WaveManager
         /// </summary>
         /// <param name="type">The type of enemy to spawn.</param>
         /// <returns>Returns a reference to the spawned GameObject.</returns>
-        private WaveEnemy SpawnEnemy(EnemyType type)
+        private WaveEnemy SpawnEnemy(int id)
         {
             // Spawn the enemy and save a reference to it for later manipulation by functions subscribed to the event.
             WaveEnemy enemy = new WaveEnemy
             {
-                obj = GameObject.Instantiate(prefabs[(int)type], transform.position, Quaternion.identity)
+                obj = GameObject.Instantiate(prefabs[id])
             };
-            
+
             // Invoke the proper event for spawning an enemy.
             OnSpawnEnemy?.Invoke(enemy);
 
@@ -267,8 +279,8 @@ namespace xtilly5000.Prototypes.WaveManager
     [System.Serializable]
     public class WaveEnemy
     {
-        // The type of this enemy.
-        public EnemyType type;
+        // The id of the enemy. This is NOT a unique value!
+        public int id;
 
         // The wave that this enemy spawned in.
         public Wave wave;
@@ -284,17 +296,66 @@ namespace xtilly5000.Prototypes.WaveManager
     }
     #endregion
 
-    #region EnemyType Enum
+    #region Step Class
     /// <summary>
-    /// EnemyType stores a list of all the different enemy types.
+    /// This class provides the basic data structure for creating steps.
     /// </summary>
     [System.Serializable]
-    public enum EnemyType
+    public class Step
     {
-        Walker = 0,
-        Crawler = 1,
-        Seeker = 2,
-        Tank = 3
+        #region StepTimingData Struct
+        [System.Serializable]
+        public struct StepTimingData
+        {
+            // How many seconds until the next step starts.
+            [Tooltip("How many seconds until the next step starts.")]
+            public float timeUntilNextStep;
+
+            // The minimum amount of spacing between enemies in seconds.
+            [Tooltip("The minimum amount of spacing between enemies in seconds.")]
+            public float minSpacing;
+
+            // The maximum amount of spacing between enemies in seconds.
+            [Tooltip("The maximum amount of spacing between enemies in seconds.")]
+            public float maxSpacing;
+
+            // Do we want to wait until the enemies are killed before moving onto the next step?
+            [Tooltip("Do we want to wait until the enemies are killed before moving onto the next step?")]
+            public bool waitUntilKill;
+        }
+        #endregion
+
+        #region StepEnemyData Struct
+        [System.Serializable]
+        public struct StepEnemyData
+        {
+            // The id of the enemy that we want to spawn.
+            [Tooltip("The id of the enemy that we want to spawn.")]
+            public int id;
+
+            // The maximum number of enemies we want to spawn in this step.
+            [Tooltip("The maximum number of enemies we want to spawn in this step.")]
+            public int maxNumberOfEnemies;
+
+            // The minimum number of enemies we want to spawn in this step.
+            [Tooltip("The minimum number of enemies we want to spawn in this step.")]
+            public int minNumberOfEnemies;
+        }
+        #endregion
+
+        #region Variables
+        // The timing data for the current step.
+        [Tooltip("The timing data for the current step.")]
+        public StepTimingData timingData;
+
+        // The enemy data for the current step.
+        [Tooltip("The timing data for the current step.")]
+        public StepEnemyData enemyData;
+
+        // The chance that this step will be skipped entirely as a percentage.
+        [Tooltip("The chance that this step will be skipped entirely as a percentage.")]
+        public int skipChance;
+        #endregion
     }
     #endregion
 }
--- Assets/One vs One (Multiplayer PvE Version)/Scripts/WaveStartManager.cs
+++ Assets/One vs One (Multiplayer PvE Version)/Scripts/WaveStartManager.cs
@@ -1,40 +1,119 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿// Copyright (c) 2020 xtilly5000
+// Licensed under the Creative Commons Attribution-ShareAlike 4.0 International license.
+// Share — copy and redistribute the material in any medium or format.
+// Adapt — remix, transform, and build upon the material for any purpose, even commercially.
+
 using UnityEngine;
-using xtilly5000.Prototypes.WaveManager;
 
-public class WaveStartManager : MonoBehaviour
+namespace xtilly5000.Prototypes.WaveManager
 {
-    int waveCount;
-    void Start()
+    #region WaveSpawnerTest Class
+    /// <summary>
+    /// WaveSpawnerTest class is used for testing the WaveManager.
+    /// It can be modified to serve a wide variety of purposes.
+    /// </summary>
+    public class WaveSpawnerTest : MonoBehaviour
     {
-        StartCoroutine(WaveManager.Instance.SpawnWave(0));
+        #region Variables
+        // Spawn the enemies with the proper location and rotation.
+        public GameObject spawnPoint;
 
-        WaveManager.OnWaveKilled += _OnWaveKilled;
-        WaveManager.OnSpawnedStep += _OnSpawnedStep;
-        WaveManager.OnSpawnedWave += _OnSpawnedWave;
-        WaveManager.OnSpawnEnemy += _OnSpawnEnemy;
-        WaveManager.OnSpawnStep += _OnSpawnStep;
-        WaveManager.OnSpawnWave += _OnSpawnWave;
-    }
+        // Keeps track of the current wave.
+        public int currentWave = 0;
 
-    void _OnWaveKilled(Wave wave){
-        Debug.Log("OnWaveKilled.");
-    }
+        // Do we want to pause the wave timer?
+        public bool pause = false;
 
-    void _OnSpawnedStep(Step step){
-        Debug.Log("OnSpawnedStep.");
+        // The amount of time between wave spawns in seconds.
+        public float timeBetweenWaves = 0f;
+
+        // The amount of time left before the next wave starts in seconds.
+        private float timeLeft = 0f;
+
+        // Lets us know if the wave we spawned was killed or not.
+        private bool waveKilled = false;
+        #endregion
+
+        #region Start() Method
+        private void Start()
+        {
+            // Register all of the event functions for later use.
+            WaveManager.OnStepFinishedSpawning += OnStepFinishedSpawning;
+            WaveManager.OnWaveFinishedSpawning += OnWaveFinishedSpawning;
+            WaveManager.OnSpawnEnemy += OnSpawnEnemy;
+            WaveManager.OnStepStartSpawning += OnStepStartSpawning;
+            WaveManager.OnWaveStartSpawning += OnWaveStartSpawning;
+            WaveManager.OnWaveKilled += OnWaveKilled;
+
+            // Start spawning the first wave!
+            StartCoroutine(WaveManager.Instance.SpawnWave(currentWave));
+        }
+        #endregion
+
+        #region Update() Method
+        private void Update()
+        {
+            // Constantly count down the time left until the next wave, unless the countdown timer is paused.
+            if(timeLeft > 0f && pause == false && waveKilled == true)
+            {
+                timeLeft -= 1 * Time.deltaTime;
+            } else if(pause == false && waveKilled == true)
+            {
+                // Time ran out, so we want to spawn the next wave.
+                timeLeft = 0f;
+                waveKilled = false;
+                StartCoroutine(WaveManager.Instance.SpawnWave(currentWave));
+            }
+        }
+        #endregion
+
+        #region OnSpawnedStep() Method
+        private void OnStepFinishedSpawning(Step step)
+        {
+            // Triggers when a step finishes spawning.
+        }
+        #endregion
+
+        #region OnSpawnedWave() Method
+        private void OnWaveFinishedSpawning(Wave wave)
+        {
+            // Triggers when a wave finishes spawning.
+        }
+        #endregion
+
+        #region OnSpawnEnemy() Method
+        private void OnSpawnEnemy(WaveEnemy enemy)
+        {
+            // Triggers when an enemy is spawned.
+            // We want to set the proper position and rotation for the spawned enemy.
+            enemy.obj.transform.position = spawnPoint.transform.position;
+            enemy.obj.transform.rotation = spawnPoint.transform.rotation;
+        }
+        #endregion
+
+        #region OnSpawnStep() Method
+        private void OnStepStartSpawning(Step step)
+        {
+            // Triggers when a step starts spawning.
+        }
+        #endregion
+
+        #region OnSpawnWave() Method
+        private void OnWaveStartSpawning(Wave wave)
+        {
+            // Triggers when a wave starts spawning.
+        }
+        #endregion
+
+        #region OnWaveKilled() Method
+        private void OnWaveKilled(Wave wave)
+        {
+            // Triggers when all enemies in a wave are killed.
+            currentWave++;
+            waveKilled = true;
+            timeLeft = timeBetweenWaves;
+        }
+        #endregion
     }
-    void _OnSpawnedWave(Wave wave){
-        Debug.Log("OnSpawnedWave.");
-    }
-    void _OnSpawnEnemy(WaveEnemy waveEnemy){
-        Debug.Log("OnSpawnEnemy.");
-    }   
-    void _OnSpawnStep(Step step){
-        Debug.Log("OnSpawnStep.");
-    } 
-    void _OnSpawnWave(Wave wave){
-        Debug.Log("OnSpawnWave.");
-    }
+    #endregion
 }
